2dec3688a8e5ece9146e50e170151d42ee1e031c
Grainier Perera <grainier.perera@macrometa.com>
2024-10-08 10:37:46 +0530

    Fix launcher for debian

--- rdp_handler/launcher.py
+++ rdp_handler/launcher.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import sys
 
@@ -6,33 +5,14 @@ from .logger import logger
 
 
 def launch_thincast(rdp_file: str):
-    if sys.platform == 'darwin':
-        try:
-            logger.info(f"Launching ThinCast client with RDP file: {rdp_file}")
+    logger.info(f"Launching ThinCast client with RDP file: {rdp_file}")
+    try:
+        if sys.platform == 'darwin':
+            # MacOS
             subprocess.Popen(["open", "-a", "Thincast Remote Desktop Client", rdp_file])
-        except Exception as e:
-            logger.error(f"Failed to launch ThinCast client: {e}")
-            raise
-    else:
-        # Possible paths for ThinCast client
-        possible_paths = [
-            '/usr/bin/thincast-client',  # Common Linux path
-            '/usr/local/bin/thincast-client',
-        ]
-
-        thincast_path = None
-        for path in possible_paths:
-            if os.path.exists(path):
-                thincast_path = path
-                break
-
-        if thincast_path is None:
-            logger.error("ThinCast RDP client not found.")
-            raise FileNotFoundError("ThinCast RDP client not found.")
-
-        try:
-            logger.info(f"Launching ThinCast client with RDP file: {rdp_file}")
-            subprocess.Popen([thincast_path, rdp_file])
-        except Exception as e:
-            logger.error(f"Failed to launch ThinCast client: {e}")
-            raise
+        else:
+            # Debian
+            subprocess.Popen(["thincast-client", rdp_file])
+    except Exception as e:
+        logger.error(f"Failed to launch ThinCast client: {e}")
+        raise
