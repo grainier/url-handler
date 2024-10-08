30d0a478098a631bfbdf12ac256a91562c10128d
Grainier Perera <grainier.perera@macrometa.com>
2024-10-08 10:18:08 +0530

    Update build_debian_installer.sh

--- build_debian_installer.sh
+++ build_debian_installer.sh
@@ -4,6 +4,8 @@ set -e
 # Install required libraries
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv python3-poetry
+python3 -m venv venv
+source venv/bin/activate
 pip3 install pyinstaller
 poetry install
 
